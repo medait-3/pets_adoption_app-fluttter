31183730cdeb8138ca0cb111c8aea4ebd7a1969e
meait-3 <tchikh3@@gmail.com>
2025-08-21 23:55:05 +0100

    final1

--- lib/main.dart
+++ lib/main.dart
@@ -122,7 +122,7 @@ class _MyAppState extends State<MyApp> {
       await file.writeAsBytes(await pdf.save());
               var snackBar = SnackBar(content: Text('save'));
         ScaffoldMessenger.of(context).showSnackBar(snackBar);
-//add/
+//add/2
       // showPrintedMessage('success', 'saved to documents');
     } catch (e) {
               var snackBar = SnackBar(content: Text('Hello World'));
